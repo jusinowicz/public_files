6f8edc6f5a75bf5b91e67413dc2dc235cfa54e1a
Jacob Usinowicz <31281754+jusinowicz@users.noreply.github.com>
2024-10-14 13:55:39 -0700

    Updated layers on map

---
+++ qgis2web_2024_10_14-13_55_10_111747/layers/LowRiskSitesNotMonitored_13.js
@@ -0,0 +1,1 @@
+var json_LowRiskSitesNotMonitored_13 = {"type":"FeatureCollection","name":"LowRiskSitesNotMonitored_13","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"Monitoring Schedule":"No monitoring park closed","Water Body ":"159"},"geometry":{"type":"Point","coordinates":[-117.8125,46.6311]}}]}
---
+++ qgis2web_2024_10_14-13_55_10_111747/styles/NHD_WA_Snake_3_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_NHD_WA_Snake_3 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var value = ""
+    var labelText = "";
+    size = 0;
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 8;
+    var offsetY = 3;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        fill: new ol.style.Fill({color: 'rgba(14,185,237,0.7764705882352941)'}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
